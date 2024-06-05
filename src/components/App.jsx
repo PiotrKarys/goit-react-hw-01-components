bab5a1aad51efb6846681527f6123c72c118558c
Piotrek <Piotr.karys96@gmail.com>
2024-06-06 01:46:43 +0200

    profile+ stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import React from 'react';
+import Profile from './profile/profile';
+import Statistics from './stats/statistics';
+import user from './profile/user.json';
+import data from './stats/data.json';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
